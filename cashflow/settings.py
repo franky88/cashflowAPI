d991b7b0ac0f952da947923c62c84906bde1e8e1
Franklin Ramos <ramosfp99@gmail.com>
2025-05-28 12:18:28 +0800

    update static and media root in settings

--- cashflow/settings.py
+++ cashflow/settings.py
@@ -126,6 +126,10 @@ TIME_ZONE = 'Asia/Manila'
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
